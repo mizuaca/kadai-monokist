2761279f996b32698594cd42a51969cfb51e8a46
mizuaca <mizuaca@gmail.com>
2018-01-07 03:05:13 +0000

    want

---
+++ monokist/app/Http/Controllers/UsersController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class UsersController extends Controller
+{
+    public function show($id)
+    {
+        $user = \App\User::find($id);
+        $count_want = $user->want_items()->count();
+        $items = \DB::table('items')->join('item_user', 'items.id', '=', 'item_user.item_id')->select('items.*')->where('item_user.user_id', $user->id)->distinct()->groupBy('items.id')->paginate(20);
+
+        return view('users.show', [
+            'user' => $user,
+            'items' => $items,
+            'count_want' => $count_want,
+            'count_have' => $count_have,
+        ]);
+    }
+}
